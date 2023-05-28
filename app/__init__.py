6e550171099788370d02a97598fe1e96f15285c9
Mohammed Faraaz Ahmed <91116035+mfaraazahmed@users.noreply.github.com>
2023-05-28 17:08:53 +0530

    Created an app to prevent PAN card tampering
    pushing the code to github

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config["ENV"] = "development"  # Set the 'ENV' key explicitly
+
+if app.config["ENV"] == "production":
+    app.config.from_object("config.DevelopmentConfig")
+elif app.config["ENV"] == "testing":
+    app.config.from_object("config.TestingConfig")
+else:
+    app.config.from_object("config.ProductionConfig")
+
+from app import views
